18bc9da2fb7dd51b2477f6ef1e2e4c5b57942b13
Ilya Raketa <paketa1337@gmail.com>
2025-06-17 06:49:15 +0300

    part1

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,14 +1,14 @@
 declare module '*.scss' {
-    const content: { [className: string]: string }
-    export default content
+    const content: { [className: string]: string };
+    export default content;
 }
 
 declare module '*.svg' {
-    const content: any
-    export default content
+    const content: any;
+    export default content;
 }
 
 declare module '*.png' {
-    const value: string
-    export default value
+    const value: string;
+    export default value;
 }
