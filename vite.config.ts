e1c0efceda39d1fa458846df8e56d11d02270012
Ilya Raketa <paketa1337@gmail.com>
2025-06-20 19:28:49 +0300

    fix styles imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
     plugins: [react()],
     css: {
         preprocessorOptions: {
             scss: {
-                additionalData: `@use "./src/styles/variables" as *;`,
+                additionalData: `@use "@/styles/variables" as *;`,
             },
         },
     },
-    build: {
-        outDir: 'dist',
-        assetsDir: 'assets',
-        emptyOutDir: true,
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
 });
