94349ff3c86ad050c71e8bfd7d065feb04102a21
Ilya Raketa <paketa1337@gmail.com>
2025-06-20 19:18:16 +0300

    fix vercel styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "./src/styles/variables" as *;`,
+            },
+        },
+    },
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        emptyOutDir: true,
+    },
 });
